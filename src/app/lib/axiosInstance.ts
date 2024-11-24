5d73cc3623b5f5c7992678ab030c397a0a0f15a1
Patipat <angpaopao55@gmail.com>
2024-11-24 14:13:44 +0700

    refactor path

--- src/app/lib/axiosInstance.ts
+++ src/app/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/',
+  baseURL: 'https://book-store-api-4mpr.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
